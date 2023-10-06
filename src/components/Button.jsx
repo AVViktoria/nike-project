43cd3e32c40cf1786a2bf662eb3f365f91628608
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-10-06 01:37:46 -0700

    feat: create CustomerReviews and Subscribe sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,16 +4,16 @@ const Button = ({
   backgroundColor,
   borderColor,
   textColor,
+  fullWidth,
 }) => {
   return (
     <button
-      className={`flex justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
-    ${
-      backgroundColor
-        ? `${backgroundColor} ${borderColor} ${textColor}`
-        : "bg-coral-red  text-white border-coral-red"
-    } rounded-full 
-    `}
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
       {iconUrl && (
