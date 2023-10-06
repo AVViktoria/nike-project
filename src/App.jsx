1cb7549d47c2dd3bc89b8131a75c7342a5d6965d
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-10-05 23:42:46 -0700

    feat: create Nav, Hero, ShoeCard

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import { Hero,
   SpecialOffer,
   CustomerReviews,
   Subscribe,
-  Footer} from "./sections";
+  Footer
+} from "./sections";
+  import Nav from './components/Nav'
 
 const App = () => {
   return (
     <main className="relative">
-      {/* <Nav/> */}
-      <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
+      <Nav/>
+      <section className="xl:padding-l wide:padding-r padding-b"><Hero/></section>
       <section className="padding"><PopularProducts/></section>
       <section className="padding"><SuperQuality/></section>
       <section className="padding padding-x py-20"><Services/></section>
